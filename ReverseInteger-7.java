588cf3071786a8d750ba197748ed75d0a94ecb3d
shelbys555 <shelkeprathamesh0@gmail.com>
2025-09-11 23:12:45 +0530

    {DSA 1/100: Solved Reverse Integer problem}

---
+++ ReverseInteger-7.java
@@ -0,0 +1,17 @@
+package DSA;
+
+class Solution {
+    public int reverse(int x) {
+        int revNo = 0;
+        int lastDigit = 0;
+        while (x != 0){
+            lastDigit = x % 10;
+            if(revNo > Integer.MAX_VALUE/10 || revNo < Integer.MIN_VALUE/10) {
+                return 0;
+            }
+            revNo = (revNo * 10) + lastDigit;
+            x /= 10;
+        }
+        return revNo;
+    }
+}
