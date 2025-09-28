8ee8800a64ca759747443283cbe2075e55c0c3ff
shelbys555 <shelkeprathamesh0@gmail.com>
2025-09-28 21:54:15 +0530

    {Day 18/100 of DSA}

---
+++ POTD976.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        int n  = nums.length;
+        int Perimeter = 0;
+        for(int i = n - 1; i >= 2; i--){
+            if(nums[i] < nums[i - 1] + nums[i - 2]){
+                Perimeter = Math.max(Perimeter,nums[i] + nums[i - 1] + nums[i - 2]);
+            }
+        }
+        return Perimeter;
+    }
+}
