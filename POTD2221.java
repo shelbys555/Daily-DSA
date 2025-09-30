3807d2fbb7d8fed5f305b2d715e21190c22a4662
shelbys555 <shelkeprathamesh0@gmail.com>
2025-09-30 23:03:53 +0530

    {Day 20/100 of DSA}

--- POTD2221.java
+++ POTD2221.java
@@ -1,12 +1,23 @@
+// class Solution {
+//     public int triangularSum(int[] nums) {
+//         int n = nums.length;
+//         while(n > 1){
+//             for(int i = 0; i < n - 1; i++){
+//                 nums[i] = (nums[i] + nums[i + 1]) % 10;
+//             }
+//             n--;
+//         }
+//         return nums[0];
+//     }
+// }
+
 class Solution {
-    public int triangularSum(int[] nums) {
-        int n = nums.length;
-        while(n > 1){
-            for(int i = 0; i < n - 1; i++){
-                nums[i] = (nums[i] + nums[i + 1]) % 10;
-            }
-            n--;
-        }
-        return nums[0];
-    }
+    public int triangularSum(int[] nums) { 
+        int n = nums.length; 
+        if(n == 1) return nums[0]; 
+        int newNums[] = new int[n - 1]; 
+        for(int i = 0; i < n - 1; i++) { 
+            newNums[i] = (nums[i] + nums[i + 1]) % 10; 
+        } 
+        return triangularSum(newNums); }
 }
